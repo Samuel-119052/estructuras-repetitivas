6c636fc346357ef2e27c9e07aaa429483f9d0aaf
Samuel Gómez <samusmusic18@gmail.com>
2025-05-30 11:17:51 -0500

    Mejora de interfaz y validacion en el ejercicio 11

--- Ejercicio11.cpp
+++ Ejercicio11.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 int main(){
 	int num, fact=1;
-	cout<<Ingrese un numero positivo<<endl;
+	cout<<"PROGRAMA INCIADO"<<endl;
+	cout<<"****************"<<endl;
+	cout<<"Ingrese un numero positivo"<<endl;
 	cin>>num;
 	if(num>=1){
 		for(int i=1;i<=num;i++){
 			fact=fact*i;
 		}
-	cout<<El factorial de su numero ingresado es: <<fact<<endl;
+	    cout<<"El factorial de su numero ingresado es: "<<fact<<endl;
+	}else{
+		cout<<"Error: Numero invalido"<<endl;
 	}
+	cout<<"___________________"<<endl;
+	cout<<"PROGRAMA FINALIZADO"<<endl;
 	return 0;
 }
