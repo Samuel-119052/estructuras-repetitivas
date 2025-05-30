fa283d8c4801c2657f102ffbb386895ac8cda503
Samuel Gómez <samusmusic18@gmail.com>
2025-05-29 20:44:43 -0500

    Agrega ejercicio 1 de estructuras repetitivas

---
+++ Ejercicio1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<Ingrese el numero de multiplos de 7 que desea imprimir<<endl;
+	cin>>n;
+	if(n>0){
+		cout<<Los primeros << n <<  multiplos de 7 son: <<endl;
+		for(int i=1;i<=n;i++){
+		cout<< i*7 <<endl;
+		}
+	}else{
+		cout<<digite otro numero que no sea negativos o cero<<endl;
+	}
+	return 0;
+}
