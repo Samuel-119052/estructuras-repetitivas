d98c1b2a47fb360c0a2f92cf0cdf9b959aa28bd8
Samuel Gómez <samusmusic18@gmail.com>
2025-05-29 23:03:13 -0500

    Mejora en el ejercicio 1 con texto y en el ejercicio 3 una validacion

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
 	int n;
-	cout<<Ingrese el numero de multiplos de 7 que desea imprimir<<endl;
+	cout<<"Ingrese el numero de multiplos de 7 que desea imprimir"<<endl;
 	cin>>n;
 	if(n>0){
-		cout<<Los primeros << n <<  multiplos de 7 son: <<endl;
+		cout<<"Los primeros "<< n <<"  multiplos de 7 son: "<<endl;
 		for(int i=1;i<=n;i++){
 		cout<< i*7 <<endl;
 		}
 	}else{
-		cout<<digite otro numero que no sea negativos o cero<<endl;
+		cout<<"digite otro numero que no sea negativos o cero"<<endl;
 	}
 	return 0;
 }
--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 int main(){
 	int n,S=0;
-	cout<<Ingrese el numero de terminos a sumar<<endl;
+	cout<<"Ingrese el numero de terminos a sumar"<<endl;
 	cin>>n;
-	for(int i=1;i<=n;i++){
+	if(n<=0){
+		cout<<"Numero invalido"<<endl;
+	}else{
+		for(int i=1;i<=n;i++){
 		S=S+i;
 	}
-	cout<<La suma de los  << n <<  primeros numeros es:<<S<<endl;
+	cout<<"La suma de los  "<< n <<"  primeros numeros es: "<<S<<endl;
+	}
 	return 0;
 }
