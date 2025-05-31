0fcd33c541e41da7d5fe52002a7e2bb1a4ccb4d9
Samuel Gómez <samusmusic18@gmail.com>
2025-05-31 09:07:55 -0500

    Mejora de validacion de numero fibonacci del ejercicio 16 de repetitivas

--- Ejercicio16.cpp
+++ Ejercicio16.cpp
@@ -2,14 +2,18 @@
 using namespace std;
 int main(){
 	int n,a=0,b=1,c;
-	cout<<Ingrese cuantos terminos de la serie fibonacci quiere ver<<endl;
+	cout<<"Ingrese cuantos terminos de la serie fibonacci quiere ver"<<endl;
 	cin>>n;
-	cout<<La serie fibonacci es:<<endl;
-	for(int i=1;i<=n;i++){
-		cout<< a <<endl;
-		c=a+b;
-		a=b;
-		b=c;
+	if(n>0){
+		cout<<"La serie fibonacci es: "<<endl;
+	    for(int i=1;i<=n;i++){
+		    cout<< a <<endl;
+		    c=a+b;
+		    a=b;
+		    b=c;
+	    }
+	}else{
+		cout<<"Erro: Se dectecto un numero invalido"<<endl;
 	}
 	return 0;
 }
