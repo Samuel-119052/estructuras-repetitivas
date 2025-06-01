25b0a872bcb75837dc25218c54ccee2c9d3b322b
Samuel Gómez <samusmusic18@gmail.com>
2025-06-01 08:45:39 -0500

    Mejora de validacion de numero negativo del ejercicio 10 de estructuras repetitivas

--- Ejercicio10.cpp
+++ Ejercicio10.cpp
@@ -2,18 +2,22 @@
 using namespace std;
 int main(){
 	int num, cont=0,i=1;
-	cout<<Ingrese un numero positivo<<endl;
+	cout<<"Ingrese un numero positivo"<<endl;
 	cin>>num;
-	while(i<=num){
-		if(num%i==0){
-			cont++;
-		}
-		i++;
-	}
-	if(cont==2){
-		cout<<Su numero ingresado es primo<<endl;
+	if(num>0){
+		while(i<=num){
+		    if(num%i==0){
+			    cont++;
+		    }
+		    i++;
+	    }
+	    if(cont==2){
+		    cout<<"Su numero ingresado es primo"<<endl;
+	    }else{
+		    cout<<"Su numero ingresado no es primo"<<endl;
+	    }
 	}else{
-		cout<<Su numero ingresado no es primo<<endl;
+		cout<<"ERROR: Numero invalido"<<endl;
 	}
 	return 0;
 }
