380923ded198d915b75be42d5e22d047e6f841b2
Samuel Gómez <samusmusic18@gmail.com>
2025-06-02 09:48:25 -0500

    Mejora de interfaz en el ejercicio 13 de estructuras repetitivas

--- Ejercicio13.cpp
+++ Ejercicio13.cpp
@@ -2,13 +2,19 @@
 using namespace std;
 int main(){
 	int num1,num2,M=0;
-	cout<<Ingrese el primer numero para la multiplicacion<<endl;
+	cout<<"_________________"<<endl;
+	cout<<"PROGRAMA INICIADO"<<endl;
+	cout<<"*****************"<<endl;
+	cout<<"Ingrese el primer numero para la multiplicacion"<<endl;
 	cin>>num1;
-	cout<<Ingrese el segundo numero para la multiplicacion<<endl;
+	cout<<"Ingrese el segundo numero para la multiplicacion"<<endl;
 	cin>>num2;
 	for(int i=1;i<=num2;i++){
 		M=M+num1;
 	}
-	cout<<La multiplicacion de los dos numeros es: <<M<<endl;
+	cout<<"La multiplicacion de los dos numeros es: "<<M<<endl;
+	cout<<"___________________"<<endl;
+	cout<<"ORIGRAMA FINALIZADO"<<endl;
+	cout<<"*******************"<<endl;
 	return 0;
 }
